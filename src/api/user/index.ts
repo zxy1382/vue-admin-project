2fbec540249190f998410e9a9ca94a2c63b11e9e
sinoso-zxy <929899958@qq.com>
2023-06-06 17:04:17 +0800

    feat: api接口管理

---
+++ src/api/user/index.ts
@@ -0,0 +1,13 @@
+import request from '@/utils/request';
+import type { loginType, loginResponseData, userResponseData } from './type';
+
+enum API {
+  LOGIN_URL = '/user/login',
+  USERINFO_URL = '/user/info',
+}
+
+export const reqLogin = (data: loginType) =>
+  request.post<any, loginResponseData>(API.LOGIN_URL, data);
+
+export const reqUserInfo = () =>
+  request.get<any, userResponseData>(API.USERINFO_URL);
---
+++ src/api/user/type.ts
@@ -0,0 +1,35 @@
+export interface loginType {
+  username: string;
+  password: string;
+}
+
+interface dataType {
+  token: string;
+}
+
+export interface loginResponseData {
+  code: number;
+  data: dataType;
+}
+
+//用户信息数据类型
+interface userInfo {
+  userId: number;
+  avatar: string;
+  username: string;
+  password: string;
+  desc: string;
+  roles: string[];
+  buttons: string[];
+  routes: string[];
+  token: string;
+}
+
+interface user {
+  checkUser: userInfo;
+}
+
+export interface userResponseData {
+  code: number;
+  data: user;
+}
