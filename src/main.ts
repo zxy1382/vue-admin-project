c14b14442e0b6a179a4c2a58bf471c34dc76893d
sinoso-zxy <929899958@qq.com>
2023-06-06 22:05:32 +0800

    feat: 静态登录页搭建

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from 'vue';
-import './style.css';
 import App from './App.vue';
 import 'element-plus/dist/index.css';
 //svg插件的配置代码
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,74 @@
 <template>
-  <div>
-    <h1>登录页面</h1>
+  <div class="login_container">
+    <el-row>
+      <el-col :span="12" :xs="0"></el-col>
+      <el-col :span="12" :xs="24">
+        <el-form class="login_form">
+          <h1>Hello</h1>
+          <h2>欢迎来到东方甄选</h2>
+          <el-form-item>
+            <el-input
+              :prefix-icon="User"
+              v-model="loginForm.username"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input
+              type="password"
+              :prefix-icon="Lock"
+              show-password
+              v-model="loginForm.password"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button class="login_btn" type="primary" size="default">
+              登录
+            </el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue';
+import { User, Lock } from '@element-plus/icons-vue';
 
-<style scoped></style>
+const loginForm = reactive({ username: 'admin', password: '111111' });
+</script>
+
+<style scoped lang="scss">
+.login_container {
+  width: 100%;
+  height: 100vh;
+  background: url('@/assets/images/background.jpg') no-repeat;
+  background-size: cover;
+}
+.login_form {
+  width: 80%;
+  top: 30vh;
+  position: relative;
+  background: url('@/assets/images/login_form.png') no-repeat;
+  background-size: cover;
+  padding: 40px;
+  h1 {
+    color: #fff;
+    font-size: 40px;
+  }
+  h2 {
+    color: #fff;
+    font-size: 20px;
+    margin: 20px 0;
+  }
+  .login_btn {
+    width: 100%;
+  }
+}
+@media (width<=768px) {
+  .login_form {
+    width: 90%;
+    margin: 0 20px;
+  }
+}
+</style>
