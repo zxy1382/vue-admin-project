04be8915e55871e460955abf74b875ea70da3ec8
sinoso-zxy <929899958@qq.com>
2023-06-06 15:46:04 +0800

    feat: 封装request

---
+++ src/utils/request.ts
@@ -0,0 +1,48 @@
+import axios from 'axios';
+// import { ElMessage } from 'element-plus';
+//用axios的create方法，创建axios实例
+const request = axios.create({
+  baseURL: import.meta.env.VITE_APP_BASE_API,
+  timeout: 5000,
+});
+//request实例添加请求拦截器
+request.interceptors.request.use((config) => {
+  //config配置对象，headers属性请求头，经常给服务器端携带公共参数
+  return config;
+});
+
+//响应拦截器
+request.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    let msg = '';
+    const status = error.response.status;
+    switch (status) {
+      case 401:
+        msg = 'token过期';
+        break;
+      case 403:
+        msg = '无权访问';
+        break;
+      case 404:
+        msg = '请求地址错误';
+        break;
+      case 500:
+        msg = '服务器出现问题';
+        break;
+      default:
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        msg = '无网络';
+        break;
+    }
+    // ElMessage({
+    //   type: 'error',
+    //   message: msg,
+    // });
+    return Promise.reject(error);
+  },
+);
+
+export default request;
