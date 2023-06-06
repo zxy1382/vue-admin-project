dd8bf39fb861db2c6ec82d3f9e2a1b17ef7d94cc
sinoso-zxy <929899958@qq.com>
2023-06-06 15:46:57 +0800

    feat: 暂存

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,25 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { Plus } from '@element-plus/icons-vue';
+// import { ElMessage } from 'element-plus';
 
 defineProps<{ msg: string }>();
 
-const count = ref(0);
+const xxx = () => {
+  // ElMessage({
+  //   type: 'error',
+  //   message: 'xxxx',
+  // });
+};
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code>
-      to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">
-      create-vue
-    </a>
-    , the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <el-button type="primary" :icon="Plus" @click="xxx">按钮</el-button>
+  <svg-icon name="phone" width="200px" height="200px"></svg-icon>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
+<style scoped lang="scss">
+h1 {
+  color: $color;
 }
 </style>
