81c6f7f86dd7ff2afb2989e1633febb54b219702
sinoso-zxy <929899958@qq.com>
2023-06-07 14:22:36 +0800

    feat: 完成登录表单校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,17 +3,22 @@
     <el-row>
       <el-col :span="12" :xs="0"></el-col>
       <el-col :span="12" :xs="24">
-        <el-form class="login_form">
+        <el-form
+          class="login_form"
+          :model="loginForm"
+          :rules="rules"
+          ref="loginFormRef"
+        >
           <h1>Hello</h1>
           <h2>欢迎来到东方甄选</h2>
-          <el-form-item>
+          <el-form-item prop="username">
             <el-input
               :prefix-icon="User"
               clearable
               v-model="loginForm.username"
             ></el-input>
           </el-form-item>
-          <el-form-item>
+          <el-form-item prop="password">
             <el-input
               type="password"
               :prefix-icon="Lock"
@@ -45,6 +50,7 @@ import { User, Lock } from '@element-plus/icons-vue';
 import useUserStore from '@/store/modules/user';
 import { useRouter } from 'vue-router';
 import { ElNotification } from 'element-plus';
+import type { FormRules } from 'element-plus';
 import { getTime } from '@/utils/time';
 
 const loading = ref(false);
@@ -52,9 +58,12 @@ const loading = ref(false);
 const userStore = useUserStore();
 const $router = useRouter();
 
+const loginFormRef = ref();
+
 const loginForm = reactive({ username: 'admin', password: '111111' });
 
 const login = async () => {
+  await loginFormRef.value.validate();
   loading.value = true;
   //通知仓库发送登录请求
   //成功->进入首页展示数据
@@ -75,6 +84,37 @@ const login = async () => {
     });
     loading.value = false;
   }
+};
+
+const validatorUserName = (_: any, value: any, callback: any) => {
+  if (value.length < 5) {
+    callback(new Error('账号长度至少五位'));
+  } else {
+    callback();
+  }
+};
+
+const validatorPass = (_: any, value: any, callback: any) => {
+  if (value.length < 6) {
+    callback(new Error('密码长度至少五位'));
+  } else {
+    callback();
+  }
+};
+
+const rules: FormRules = {
+  username: [
+    {
+      validator: validatorUserName,
+      trigger: 'change',
+    },
+  ],
+  password: [
+    {
+      validator: validatorPass,
+      trigger: 'change',
+    },
+  ],
 };
 </script>
 
